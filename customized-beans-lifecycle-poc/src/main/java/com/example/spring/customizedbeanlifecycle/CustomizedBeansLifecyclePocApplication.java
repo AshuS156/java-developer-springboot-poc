fadaac4b0d3c0683753648b99769ce0b25e4c5c6
AshuSharma16 <ashusharma_16@hotmail.com>
2025-09-25 11:54:23 +0530

    play with customzed bean life cycle

---
+++ customized-beans-lifecycle-poc/src/main/java/com/example/spring/customizedbeanlifecycle/CustomizedBeansLifecyclePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.customizedbeanlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizedBeansLifecyclePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomizedBeansLifecyclePocApplication.class, args);
+	}
+
+}
---
+++ customized-beans-lifecycle-poc/src/test/java/com/example/spring/customizedbeanlifecycle/CustomizedBeansLifecyclePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.customizedbeanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomizedBeansLifecyclePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
