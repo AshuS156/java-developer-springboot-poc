2a872d85d7c69bb81748d554de96151f5c2ffa73
AshuSharma16 <ashusharma_16@hotmail.com>
2025-09-25 11:48:58 +0530

    relax binding in spring boot

---
+++ spring-relax-binding/src/main/java/com/example/spring/relaxbinding/SpringRelaxBindingApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.relaxbinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRelaxBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRelaxBindingApplication.class, args);
+	}
+
+}
---
+++ spring-relax-binding/src/test/java/com/example/spring/relaxbinding/SpringRelaxBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.relaxbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRelaxBindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
